432de4a6d01c7cd6d0390c1d05761d3449055dff
jluis <joseluis_pr@outlook.es>
2023-06-04 09:20:33 -0600

    pass_fail

---
+++ pass_fail.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"keyboard"
+)
+
+func main() {
+	fmt.Println("Enter a grado:" )
+	grade, err := keyboard.GetFloat ()
+	if err != nil {
+		log.Fatal(err)
+	}
+var status string 
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("a grado of", grade, "is", status)
+}
